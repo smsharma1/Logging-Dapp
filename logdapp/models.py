b82f423c26a035e7da7141c87c9696d132876a5c
rahulguptakota <grahul@iitk.ac.in>
2017-10-12 17:17:05 +0530

    created model prof

--- logdapp/models.py
+++ logdapp/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Prof(models.Model):
+    profid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+    office = models.CharField(max_length=200)
+    email = models.EmailField()
+    course = models.CharField(max_length=200)
