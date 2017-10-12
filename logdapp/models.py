cde39ee760eb4a867d2abcdbb8f00f7bbcc92b0b
Shubham <sharma.shubham736@gmail.com>
2017-10-12 17:16:29 +0530

    models

--- logdapp/models.py
+++ logdapp/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    Name = models.CharField(max_length = 200)
+    Roll_Number = models.AutoField(primary_key=True)
+    Semester = models.IntegerField()
+    Degree = models.CharField(max_length = 20)
+    Department = models.CharField(max_length = 25)
+    Email = models.EmailField()
+
+class Courses(models.Model):
+    ID = models.CharField(max_length=10,primary_key=True)
+    Name = models.CharField(max_length=20)
