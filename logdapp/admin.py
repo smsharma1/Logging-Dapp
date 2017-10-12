f5daa4b0ed9a94fc26dd7b05e4df3ff9fa5c55de
Shubham <sharma.shubham736@gmail.com>
2017-10-12 17:36:36 +0530

    ui

--- logdapp/admin.py
+++ logdapp/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Students, Courses
+admin.site.register(Students)
+admin.site.register(Courses)
+
