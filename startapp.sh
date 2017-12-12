abde990c098f696cee7af36a492b550ebc8653f2
Shubham <sharma.shubham736@gmail.com>
2017-12-12 23:31:57 +0530

    sleep added

--- startapp.sh
+++ startapp.sh
@@ -4,6 +4,7 @@ if [ ! $# == 2 ]; then
 fi
 echo "Ignore the error that accompanies if it is said that chain already exists or multichaind is already running"
 multichaind chain1@$1:$2 -daemon
+sleep 2.5
 python getpermissions.py
 echo "Note the username "
 python manage.py runserver 5000
